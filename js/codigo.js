aeb82f74a335e3cda56e3c018b987df2daffd008
Omar Soto <osjpsi@yandex.com>
2023-03-20 13:19:33 -0600

    bugfixing

--- js/codigo.js
+++ js/codigo.js
@@ -1,3 +1,36 @@
-const autor = document.getElementById("autor_input")
-const titulo = document.getElementById("titulo_input")
-const condicion = document.getElementById("condicion_input")
+const autor = document.getElementById("autorInput");
+const titulo = document.getElementById("tituloInput");
+const condicion = document.getElementById("condicionInput");
+const libros = JSON.parse(localStorage.getItem("libros")) || [];
+const tabla_resultados = document.getElementById("tabla_resultados");
+
+const agregar = () => {
+  const libro = {
+    id: crypto.randomUUID(),
+    autor: autor.value,
+    titulo: titulo.value,
+  };
+
+  libros.push(libro);
+
+  localStorage.setItem("libros", JSON.stringify(libros));
+  mostrar_libros();
+};
+
+const mostrar_libros = () => {
+  tabla_resultados.innerHTML = "";
+  libros.forEach((libro) => {
+    tabla_resultados.innerHTML += `<tr>
+        <th scope="row">${libro.id}</th>
+        <td>${libro.autor}</td>
+        <td>${libro.titulo}</td>
+        <td>${libro.condicion}</td>
+        <td>
+        <button>
+          onclick="eliminarLibros('${libro.id}')"
+        >
+          Eliminar
+        </button><td>
+    </tr>`;
+  });
+};
